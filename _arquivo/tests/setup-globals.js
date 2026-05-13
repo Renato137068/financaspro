600c961d9ea3ff4f686bc5cf5b7038ae13ed34a8
Renato Soares <renato.soares1370@gmail.com>
2026-05-13 12:20:00 -0300

    chore: limpeza de arquivos obsoletos e atualização de configuração
    - Removido demo-login.html (arquivo de teste)
    - Removido pasta _arquivo/ (backup de arquivos antigos)
    - Atualizado package.json: paths de testes para nova estrutura modular
    - Atualizado .gitignore: adicionado coverage/, *.log, .DS_Store, Thumbs.db

--- _arquivo/tests/setup-globals.js
+++
@@ -1,4 +0,0 @@
-global.CONFIG = require('../js/config.js');
-global.UTILS = require('../js/utils.js');
-global.DADOS = require('../js/dados.js');
-global.TRANSACOES = require('../js/transacoes.js');
